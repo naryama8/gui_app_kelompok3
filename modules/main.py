b1a6d5f5f5c20353abd4f47d146bd618eea82453
Hi-Keiko <keikoaurelia10@gmail.com>
2025-08-12 13:50:39 +0700

    update

--- modules/main.py
+++ modules/main.py
@@ -1,6 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5.uic import loadUi
 
 import resources_rc
 
+class Login(QDialog):
+    def __init__(self):
+        super(Login,self).__init__()
+        loadUi("loginpage.ui",self)
 
+        self.loginButton.clicked.connect(self.loginfunction)
+
+    def loginfunction(self):
+        username=self.name.text()
+        password=self.password.text()
+        print("Successfully Logged in!")
