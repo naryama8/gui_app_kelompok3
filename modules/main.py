62bdb9a321e23e4b6aa1a80cd0c4b693880caf3b
Hi-Keiko <keikoaurelia10@gmail.com>
2025-08-19 11:50:47 +0700

    login flow

--- modules/main.py
+++ modules/main.py
@@ -8,11 +8,26 @@ import resources_rc
 class Login(QMainWindow):
     def __init__(self):
         super(Login,self).__init__()
-        loadUi("loginpage.ui",self)
+        loadUi("ui_files/loginpage.ui", self)
 
         self.loginButton.clicked.connect(self.loginfunction)
+
+        # load gambar ke label
+        from PyQt5.QtGui import QPixmap
+        pixmap = QPixmap(":/images/logodompetq.png")
+        self.logo.setPixmap(pixmap)
+        self.logo.setScaledContents(True)
 
     def loginfunction(self):
         username=self.name.text()
         password=self.password.text()
         print("Successfully Logged in!")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = Login()
+    widget=QtWidgets.QStackedWidget()
+    widget.addWidget(ui)
+    widget.resize(ui.size()) 
+    widget.showMaximized()
+    sys.exit(app.exec_())
