02703c191cb119f3dde87af99a3a74c4bbdca4a3
Hi-Keiko <keikoaurelia10@gmail.com>
2025-08-24 16:27:27 +0700

    update

---
+++ modules/dashboard.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QToolTip
+import json
+import os
+from PyQt5 import QtCore
+from PyQt5.QtCore import QTimer  
+
