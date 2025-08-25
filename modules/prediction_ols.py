d5d49af781d26eb735e49fbae5c1a9a715a8353b
Hi-Keiko <keikoaurelia10@gmail.com>
2025-08-25 13:38:35 +0700

    update

--- modules/prediction_ols.py
+++ modules/prediction_ols.py
@@ -1,3 +1,4 @@
 #ini modul buat bikin prediksi dalam rentang maksimal 30 hari ke depan kalo pengguna baru masukin sedikit data
 #basicnya adalah numerical method, yaotu Ordinary Leas Square (OLS)
 #Rencananya, bakal pake polinomial regression yang digabung sama fourier regression
+
